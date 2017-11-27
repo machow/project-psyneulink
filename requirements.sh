28f9e3a3bb54b58dc68a5161560141ec923e0ca7
Rasmus Bååth <rasmus.baath@gmail.com>
2017-11-27 14:19:13 +0100

    Small fixes.

--- requirements.sh
+++ requirements.sh
@@ -1,20 +1,10 @@
 # If bash command fails, build should error out
 set -e
 
-##### Install specific package versions with pip #####
+# This bash script will be run on the Linux/Ubuntu image running your project.
+# Use it to install python packages necessary for your project.
+# and to setup the environment in other ways. 
+
+# For example, install specific package versions with pip 
 # pip3 install pandas==0.20.3
 
-
-##### Add files to the datasets/ folder on the live project #####
-# A list of files in datasets/ separated by space, needs to be manually updated
-FILES=""
-# The URL where the assets of the project lives
-ASSETURL=
-# Where the datasets will be compied on the Project image.
-DATADIR=/home/repl/datasets
-
-mkdir -p $DATADIR
-for FILE in $FILES
-do
-	wget -O $DATADIR/$FILE $ASSETURL/$FILE
-done
